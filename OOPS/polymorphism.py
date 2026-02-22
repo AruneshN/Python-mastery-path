2f8305f4f267ad3ca6fc73ccc9a20a992c7a6f9e
Arunesh.N <125756336+AruneshN@users.noreply.github.com>
2026-02-22 12:25:02 +0530

    Update polymorphism.py

--- OOPS/polymorphism.py
+++ OOPS/polymorphism.py
@@ -1,5 +1,7 @@
 """
 topics:polymorphism
+1)Method overriding
+2)method overloading
 
 desc:
 polymorphism means many forms same method using many forms
@@ -26,3 +28,49 @@ c=card()
 
 for payments in (u,c):
     print(payments.pay())
+
+
+
+
+#Method overriding
+"""
+Dis:
+having multiple methods in same name but different parameter and different signature of using
+pay have same name but different parameters
+
+"""
+
+class Payment:
+    def pay(self,amount): #parent class behaviour
+        return f'Processing payment {amount} is paided due to generic method'
+    
+class UPI(Payment):
+    def pay(self,amount): #overriding
+        return f'Processing payment {amount} is paided due to UPI'
+    
+class Net_Banking(Payment):
+    def pay(self,amount):#overriding
+        return f'Processing payment {amount} is paided due to Net_Banking'
+
+class GPAY(Payment):
+    def pay(self,amount):#overriding
+        return f'Processing payment {amount} is paided due to Gpay'
+
+Pay=UPI()
+print(Pay.pay("25000"))
+
+
+#Method overloading
+"""
+Method overloading is same class same name but different parameters.
+add is a same name same function but receive different parameters
+"""
+
+class Calculator:
+    def add(self,*nums):
+        return sum(nums)
+
+
+total=Calculator()
+print(total.add(4,6))
+print(total.add(4,6,20))
